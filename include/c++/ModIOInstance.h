560ea93c78fbe691a2a4de9fa7717483c61eadd0
turupawn <ahmed.hn.43@gmail.com>
2018-12-05 19:25:03 -0600

    removed overloaded funcion

--- include/c++/ModIOInstance.h
+++ include/c++/ModIOInstance.h
@@ -50,7 +50,6 @@ class MODIO_DLL Instance
   int current_call_id;
 
 public:
-  Instance(u32 environment, u32 game_id, const std::string &api_key);
   Instance(u32 environment, u32 game_id, const std::string &api_key, const std::string &root_path);
   ~Instance();
 
--- src/c++/ModIOInstance.cpp
+++ src/c++/ModIOInstance.cpp
@@ -2,12 +2,6 @@
 
 namespace modio
 {
-Instance::Instance(u32 environment, u32 game_id, const std::string &api_key)
-{
-  this->current_call_id = 0;
-  modioInit(environment, game_id, (char *)api_key.c_str(), NULL);
-}
-
 Instance::Instance(u32 environment, u32 game_id, const std::string &api_key, const std::string &root_path)
 {
   this->current_call_id = 0;
