df545531001b76c3b36d1c95b381150c75849ece
Ahmed Castro <ahmed.hn.43@gmail.com>
2018-12-18 14:27:01 -0600

    Update appveyor_build_config.bat

--- appveyor_build_config.bat
+++ appveyor_build_config.bat
@@ -1,10 +1,14 @@
-mkdir dynamic_build
-cd dynamic_build
-cmake -D test=on -G "Visual Studio 15" c:\projects\source
-cmake --build . --config "Release"
+mkdir test_build
+cd test_build
+cmake -D mode=static -D test=on -D gtest_force_shared_crt=on -G "Visual Studio 15" c:\projects\source
+cmake --build . --config "Debug"
 
 if %APPVEYOR_REPO_TAG% == true (
   echo "Release tag detected"
+  mkdir dynamic_build
+  cd dynamic_build
+  cmake -D test=on -G "Visual Studio 15" c:\projects\source
+  cmake --build . --config "Release"
   mkdir ..\static_build
   cd ..\static_build
   cmake -D mode=static -G "Visual Studio 15" c:\projects\source
@@ -17,8 +21,9 @@ if %APPVEYOR_REPO_TAG% == true (
   cd ..\static_build_64bits
   cmake -D mode=static -G "Visual Studio 15 Win64" c:\projects\source
   cmake --build . --config "Release"
-  cd ..
   7z a Windows.zip dynamic_build\Release\* static_build\Release\* dynamic_build_64bits\Release\* static_build_64bits\Release\*
 ) else (
   echo "Release tag not detected"
 )
+
+cd ..
